647ebfb38b7d850b50485a2a67f5c9012ab181ce
Rahul Kansara <rahulkansara@hotmail.com>
2025-09-23 04:25:58 +0000

    Prepzee

---
+++ models/silver/dim_customers.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table') }}
+
+
+SELECT
+    customer_id,
+    TRIM(UPPER(first_name)) as first_name,
+    TRIM(UPPER(last_name)) as last_name,
+    LOWER(TRIM(email)) as email,
+    TO_DATE(signup_date) as signup_date,
+    CASE
+        WHEN subscription_type = 'Premium' THEN 'PREMIUM'
+        WHEN subscription_type = 'Standard' THEN 'STANDARD'  
+        WHEN subscription_type = 'Basic' THEN 'BASIC'
+        ELSE 'UNKNOWN'
+    END as subscription_type_clean,
+    CURRENT_TIMESTAMP() as processed_at
+FROM {{ source('raw_data', 'raw_customers') }}
+WHERE email IS NOT NULL
